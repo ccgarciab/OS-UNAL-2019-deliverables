b2c106be8484631e6e748f71f6ad8741ff7ace6b
Cristian García <ccgarciab@unal.edu.co>
2019-05-04 23:19:42 -0500

    Prototype of storing dogType structs. Taking input from stdin.

--- deliverable-1/p1-dogProgram.c
+++ deliverable-1/p1-dogProgram.c
@@ -2,14 +2,58 @@
 #include <stdlib.h>
 
 #include "pet_globals.h"
+
+#include "pet_file.h"
 #include "pet_hashtable.h"
 #include "pet_input.h"
+#include "pet_output.h"
+
+#include "error_handle.h"
 
 node table[T_SIZE];
-int current = 0;
+int curr_hist = 0;
 
 int main(int argc, char* argv[]){
 
-    printf("%ld\n", sizeof(dogType));
-    printf("%ld\n", sizeof(table));
+    FILE *db = fopen(PATH, "wr+");
+
+    if(!db) sys_error("can't open file\n");
+
+    char opt[2];
+    opt[0] = 'A';
+    dogType pet;
+
+    do{
+
+        printf("opt: ");
+        if(!get_bounded_str(opt, 1)) continue;
+
+        switch(opt[0]){
+
+            case '1':
+
+                fill_pet_info(&pet);
+                int line = get_line(table, pet.name);
+                pet.doc_id = curr_hist++;
+                pet.next = -1;
+
+                if(line < 0){
+
+                    pet.prev = -1;
+                    line = append_pet(db, &pet);
+                    insert_new_line(table, pet.name, line);
+                }
+                else{
+
+                    add_pet_from_line(db, &pet, line);
+                }
+                break;
+
+            default:
+                break;
+        }
+
+    }while(opt[0] != 'q');
+
+    fclose(db);
 }
