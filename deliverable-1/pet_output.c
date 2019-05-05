9e02d74c1ccc4c5ab82e6891684959d851caaf66
Cristian García <ccgarciab@unal.edu.co>
2019-05-05 00:19:51 -0500

    Functions for browsing the medical records of pets.

--- deliverable-1/pet_output.c
+++ deliverable-1/pet_output.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include "pet_globals.h"
 #include "pet_output.h"
+
+#include "error_handle.h"
+
+#define EDITOR "nano"
 
 void print_pet(dogType *pet){
 
@@ -27,4 +33,19 @@ previous record: %d\n \
 next record: %d\n \
 **************************************************\n", 
     pet->doc_id, pet->prev, pet->next);
+}
+
+void open_medical_record(int doc_id){
+
+    pid_t pid = fork();
+
+    if(pid < 0) sys_error("fork failure\n");
+
+    if(pid) return;
+
+    char arg[30];
+    sprintf(arg, "./records/%d", doc_id);
+
+    if(execl(EDITOR, arg, NULL))
+        sys_error("exec error\n");
 }
--- deliverable-1/pet_output.h
+++ deliverable-1/pet_output.h
@@ -1,2 +1,3 @@
 void print_pet(dogType *pet);
 void print_pet_debug(dogType *pet);
+void open_medical_record(int doc_id);
