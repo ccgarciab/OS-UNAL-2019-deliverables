bd7ba2668de7c57a46f0fd4c845974510e655546
Cristian García <ccgarciab@unal.edu.co>
2019-05-07 23:00:04 -0500

    Fixing the mess caused by git-merge >:[

--- deliverable-1/p1-dogProgram.c
+++ deliverable-1/p1-dogProgram.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "pet_globals.h"
 
@@ -14,8 +15,6 @@ node table[T_SIZE];
 int curr_hist = 0;
 
 int main(int argc, char* argv[]){
-
-
 
     FILE *db = fopen(PATH, "rb+");
 
@@ -72,9 +71,6 @@ int main(int argc, char* argv[]){
 
  	   	;
            	fseek(db, 0, SEEK_END);
-
-
-
 		      
 		   int total_lines = get_total_lines();
 
@@ -133,9 +129,7 @@ int main(int argc, char* argv[]){
                                        perror("Writting Error");
                                        exit(-1);
                                    }
-
-
-
+                                   
                                    fprintf(file, "Id: %i\n", datos->doc_id);
                                    fprintf(file, "Name: %s\n", datos->name);
                                    fprintf(file, "Type:   %s\n", datos->type);
@@ -228,81 +222,6 @@ int main(int argc, char* argv[]){
                 else{
 
                     printf("pet name not found\n");
-                }
-                break;
-
-	   case '2':
-
-
-           fseek(db, 0, SEEK_END);
-           int total_lines = get_total_lines();
-           printf("Numbers of Pets inside dataDogs.dat: %d\n", total_lines);
-           printf("Please digit the number of register for data visualization: ");
-           int numPet;
-
-           if (scanf("%d", &numPet)== EOF) {
-               perror("Error in Scanf");
-               exit(-1);
-           }
-
-           if (numPet>total_lines){
-               printf("Please insert a number inside the register number inside of the Data base\n");
-           }else{
-               dogType *datos = malloc(sizeof(dogType));
-
-               fseek(db, (numPet - 1) * sizeof(dogType), SEEK_SET);
-               int readVeredict = fread(datos, sizeof(dogType), 1, db);
-
-               if (readVeredict == 0 ){
-                   perror("Read Error\n");
-                   exit(-1);
-               }
-
-               printf("-------------------------------------------------\n");
-               printf("Name: %s\n", datos->name);
-               printf("Type:   %s\n", datos->type);
-               printf("Age:   %i\n", datos->age);
-               printf("Breed:   %s\n", datos->breed);
-               printf("Size: %i\n", datos->size);
-               printf("Weight:   %f\n", datos->weight);
-               printf("Sex: %c\n", datos->sex);
-               printf("Next: %d\n", datos->next);
-               printf("-------------------------------------------------\n");
-
-               free(datos);
-           }
-	   break;
-
-            case '3':
-
-                ;
-                int n = get_total_lines();
-                printf("Number of pets in dataDogs.dat: %d\n", n);
-                int input = get_int("number of register to delete: ");
-                if(input <= 0 || n < input) break;
-                input--;
-                dogType d;
-                read_pet_at_line(db, &d, input);
-                print_pet(&d);
-                char yn[2];
-                while(1){
-                    printf("\nDo you want to delete this pet?(y/n)\n");
-                    if(!get_bounded_str(yn, 1)) continue;
-                    if(yn[0] != 'y' && yn[0] != 'n' && yn[0] != 'Y' && yn[0] != 'N')
-                        continue;
-                }
-                if(yn[0] == 'Y' || yn[0] == 'y'){
-
-                    delResult dr;
-                    del_pet(db, input, &dr);
-                    if(dr.update_del){
-
-                        update_line(table, d.name, dr.newln_del);
-                    }
-                    if(dr.update_repl){
-
-                        update_line(table, dr.word_repl, dr.newln_repl);
-                    }
                 }
                 break;
 
