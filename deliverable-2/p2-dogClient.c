09aadf8e263ea7a2a35889b96897e39f647cc29d
Cristian García <ccgarciab@unal.edu.co>
2019-05-18 21:06:12 -0500

    First draft of client - server program. Implemented option 1: sending pet from client and storing it in file at server.

--- deliverable-2/p2-dogClient.c
+++ deliverable-2/p2-dogClient.c
@@ -1,4 +1,95 @@
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <strings.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-int main(int argc, char* argv[]){
+#include "pet_globals.h"
 
+#include "error_handle.h"
+#include "pet_input.h"
+#include "pet_output.h"
+#include "sock_aux.h"
+
+#define PORT 3535
+
+struct sockaddr_in server;
+size_t size;
+int err;
+char buffer[32];
+
+int main() {
+
+	int fd = socket(AF_INET, SOCK_STREAM,0);
+	
+    if (fd == -1) sys_error("socket error");
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    bzero(server.sin_zero, 8);
+
+    size = sizeof(struct sockaddr_in);
+
+    err = connect(fd, (struct sockaddr*)&server, size);
+    if (err == -1) sys_error("connect error");
+
+    char opt[2];
+    //char name[33];
+    int ans;
+    opt[0] = '\1';
+    opt[0] = '\0';
+    dogType pet;
+
+    do {
+
+        printf("\nMenu:\n");
+        printf("1. Insert a Register\n");
+        printf("2. Visualize a Register\n");
+        printf("3. Erase a Register\n");
+        printf("4. Search a Register\n");
+        printf("5. Quit\n\n");
+
+        if (!get_bounded_str(opt, 1)) continue;
+
+        printf("\n");
+
+        switch(opt[0]){
+        
+            case '1':
+                
+                fill_pet_info(&pet);
+                pet.next = -1;
+                printf("\nThis is the register you are about to create:\n\n");
+                print_pet(&pet);
+                ans = confirmation("Do you want to create it?");
+                
+                if(ans){
+                
+                    send_full(fd, opt, 1);
+                    send_full(fd, &pet, sizeof(dogType));
+                }
+                
+                break;
+            
+            case '2':
+            
+                //TODO NEXT
+            
+                break;
+            
+            default:
+
+                send_full(fd, opt, 1);
+
+            break;
+        }
+
+    } while (opt[0] != '5');
+
+    close(fd);
 }
--- deliverable-2/p2-dogServer.c
+++ deliverable-2/p2-dogServer.c
@@ -1,4 +1,131 @@
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <strings.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[]){
+#include "pet_globals.h"
 
+#include "pet_file.h"
+#include "pet_hashtable.h"
+#include "pet_input.h"
+#include "pet_output.h"
+
+#include "error_handle.h"
+
+#define PORT 3535
+#define BACKLOG 32
+
+struct sockaddr_in server, client_I;
+size_t size;
+socklen_t sizeClient;
+int err, fd_I;
+
+node table[T_SIZE];
+int curr_hist = 0;
+
+int main () {
+
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (fd == -1){
+        perror("error in socket");	
+        exit(-1);	
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = INADDR_ANY;
+    bzero(server.sin_zero, 8);
+    size =  sizeof(struct sockaddr_in);
+
+    err = bind(fd, (struct sockaddr*)&server, size);
+    if (err == -1) sys_error("bind error");
+
+    err = listen(fd, BACKLOG);
+    if (err == -1) sys_error("listen error");
+
+    sizeClient = 0;
+
+    fd_I = accept(fd, (struct sockaddr*)&client_I, &sizeClient);
+
+    //Openning dataDogs.dat
+    FILE *db = fopen(PATH, "rb+");
+    if (!db) sys_error("can't open file\n");
+
+    //hash table init
+    int num_lines = init_table(table, db);
+    set_total_lines(num_lines);
+
+    char got;
+    char name[33];
+    int ans;
+    dogType pet;
+
+    //Updating IDs sequence number by structs number inside dataDogs.dat
+    curr_hist = ftell(db) / sizeof(dogType);
+
+    do{
+    
+        do{
+        
+            err = recv(fd_I, &got, 1, 0);
+            if(err == -1) sys_error("receive error");
+
+        }while(err == 0);
+        
+        printf("%c\n", got);
+
+        switch(got){
+        
+            case '1':
+                
+                for(int received = 0; received < sizeof(dogType);){
+                
+                    int res = recv(fd_I, (void *)(&pet) + received, sizeof(dogType) - received, 0);
+                    if(res == -1) sys_error("receive error, got 1");
+                    received += res;
+                }
+                
+                strcpy(name, pet.name);
+                word_to_upper(name);
+                pet.next = -1;
+                
+                int line = get_line(table, name);
+                
+                if (line < 0) {
+
+                    pet.prev = -1;
+                    line = append_pet(db, &pet);
+                    insert_new_line(table, name, line);
+                } else {
+
+                    add_pet_from_line(db, &pet, line);
+                }
+                
+                break;
+                
+            case '2':
+                
+                //TODO NEXT
+                
+                break;
+                
+            default:
+            
+                break;
+        }
+
+    } while(got != '5');
+
+    shutdown(fd_I, SHUT_RDWR);
+    shutdown(fd, SHUT_RDWR);
+    close(fd_I);
+    close(fd);
+
+	return 0;
 }
